aac7aca6c987c461d5931dfd99a3618e49b9fe01
Neil Kumar <neilkumar@macbookpro.mynetworksettings.com>
2024-11-03 19:18:53 -0500

    hehe

---
+++ src/components/Team.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Meet the Team</h2>
+          <p>
+          Meet our dedicated team of passionate students, each bringing unique skills and expertise to help your business thrive online. Together, we’re committed to providing exceptional service and innovative solutions tailored to your needs.
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-6 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
